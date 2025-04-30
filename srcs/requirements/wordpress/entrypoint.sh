2f0cb3c4fb846bb86775f56a9d143dc1832a0ae2
Nathan Lebrun <nalebrun@fu-r2-p1.s19.be>
2025-04-30 15:05:39 +0200

    add the env temporarlly and update to test on vm

---
+++ srcs/requirements/wordpress/entrypoint.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+set -e
+
+echo "Waiting for MySQL to be ready..."
+until wp db check --allow-root; do
+  sleep 2
+done
+
+if ! wp core is-installed --allow-root; then
+  wp core install \
+    --url="http://localhost" \
+    --title="A site" \
+    --admin_user="$WORDPRESS_ADMIN_USER" \
+    --admin_password="$WORDPRESS_ADMIN_PASSWORD" \
+    --admin_email="$WORDPRESS_ADMIN_EMAIL" \
+    --skip-email \
+    --allow-root
+fi
+
+# --- basic homepage if it doesn't exist ---
+HOMEPAGE_ID=$(wp post list --post_type=page --pagename=home --field=ID --allow-root)
+
+if [ -z "$HOMEPAGE_ID" ]; then
+  HOMEPAGE_ID=$(wp post create \
+    --post_type=page \
+    --post_title="Home" \
+    --post_name="home" \
+    --post_status=publish \
+    --porcelain \
+    --allow-root)
+fi
+
+# Set the static homepage
+wp option update show_on_front 'page' --allow-root
+wp option update page_on_front $HOMEPAGE_ID --allow-root
+
+exec "$@"
