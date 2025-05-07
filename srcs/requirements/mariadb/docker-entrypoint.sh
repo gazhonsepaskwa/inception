cf5f27dc08e2844c1200e03bfc530a06a333f977
Nathan Lebrun <nalebrun@fu-r2-p3.s19.be>
2025-05-07 15:02:21 +0200

    a

--- srcs/requirements/mariadb/docker-entrypoint.sh
+++ srcs/requirements/mariadb/docker-entrypoint.sh
@@ -5,7 +5,12 @@ DATADIR="/var/lib/mysql"
 
 mkdir -p /run/mysqld
 chown mysql:mysql /run/mysqld
-chown -R mysql:mysql /var/lib/mysql
+chown -R mysql:mysql "$DATADIR"
+
+if [ ! -d "$DATADIR/mysql" ]; then
+  echo "Database not initialized, running init.sh..."
+  /usr/local/bin/init.sh
+fi
 
 echo "Starting MariaDB server..."
 exec gosu mysql mysqld
