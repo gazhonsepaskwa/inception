e5cf3075b03c255e581d3d4940e35a5f8322c0bf
Nathan Lebrun <nalebrun@fu-r2-p3.s19.be>
2025-05-09 15:32:01 +0200

    aa

--- srcs/requirements/wordpress/docker-entrypoint.sh
+++ srcs/requirements/wordpress/docker-entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -e
 
-envsubst < /var/www/wordpress/wp-config.php.template > /var/www/wordpress/wp-config.php
+envsubst < /var/www/html/wp-config.php.template > /var/www/html/wp-config.php
 
-chown www-data:www-data /var/www/wordpress/wp-config.php
+chown www-data:www-data /var/www/html/wp-config.php
 
 sed -i 's|^listen = .*|listen = 9000|' /etc/php/7.4/fpm/pool.d/www.conf
 
