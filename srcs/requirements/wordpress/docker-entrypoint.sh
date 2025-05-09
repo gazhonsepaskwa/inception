16c040d60fca0682efa813a73f6143ef7e965cf3
Nathan Lebrun <gaz.honsepaskwa@gmail.com>
2025-05-09 15:14:50 +0200

    a

--- srcs/requirements/wordpress/docker-entrypoint.sh
+++ srcs/requirements/wordpress/docker-entrypoint.sh
@@ -5,5 +5,9 @@ envsubst < /var/www/wordpress/wp-config.php.template > /var/www/wordpress/wp-con
 
 chown www-data:www-data /var/www/wordpress/wp-config.php
 
+sed -i 's|^listen = .*|listen = 9000|' /etc/php/7.4/fpm/pool.d/www.conf
+
+mkdir /run/php/
+
 echo "Starting php srv"
-exec php-fpm7.4 -v
+exec php-fpm7.4 -F -v
