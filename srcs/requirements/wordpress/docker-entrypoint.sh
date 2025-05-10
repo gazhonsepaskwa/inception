2a5dc5d003fe36c95569b9347fc0bedb5ad01bb6
nathan lebrun <gaz.honsepaskwa@gmail.com>
2025-05-10 11:18:57 +0200

    a

--- srcs/requirements/wordpress/docker-entrypoint.sh
+++ srcs/requirements/wordpress/docker-entrypoint.sh
@@ -1,18 +1,6 @@
 #!/bin/bash
 set -e
 
-echo -n "Waiting for MariaDB to be accessible"
-until nc -z "${WORDPRESS_DB_HOST}" 3306; do
-    echo -n "."
-    sleep 2
-done
-echo "\nMariaDB is up - continuing"
-
-if [ ! -f /var/www/html/wp-config.php.template ]; then
-    echo "Error: wp-config.php.template not found in /var/www/html"
-    exit 1
-fi
-
 envsubst < /var/www/html/wp-config.php.template > /var/www/html/wp-config.php
 
 chown -R www-data:www-data /var/www/html
