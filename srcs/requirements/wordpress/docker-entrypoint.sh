a13e6fcbad3c225e79541612851a27b6f21603a5
nathan lebrun <gaz.honsepaskwa@gmail.com>
2025-05-10 12:20:58 +0200

    a

--- srcs/requirements/wordpress/docker-entrypoint.sh
+++ srcs/requirements/wordpress/docker-entrypoint.sh
@@ -1,7 +1,22 @@
 #!/bin/bash
 set -e
 
-envsubst < /var/www/html/wp-config.php.template > /var/www/html/wp-config.php
+# Generate wp-config.php using environment variables
+cat <<EOF > /var/www/html/wp-config.php
+<?php
+define('DB_NAME', getenv('WORDPRESS_DB_NAME'));
+define('DB_USER', getenv('WORDPRESS_DB_USER'));
+define('DB_PASSWORD', getenv('WORDPRESS_DB_PASSWORD'));
+define('DB_HOST', getenv('WORDPRESS_DB_HOST'));
+define('DB_CHARSET', 'utf8');
+define('DB_COLLATE', '');
+\$table_prefix = 'wp_';
+define('WP_DEBUG', false);
+if (!defined('ABSPATH')) {
+    define('ABSPATH', dirname(__FILE__) . '/');
+}
+require_once ABSPATH . 'wp-settings.php';
+EOF
 
 chown -R www-data:www-data /var/www/html
 
