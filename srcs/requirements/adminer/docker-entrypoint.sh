1b5835e9584ed12fc1d24907f20fe93ec2a81ace
nathan lebrun <gaz.honsepaskwa@gmail.com>
2025-05-13 15:23:47 +0200

    aa

--- srcs/requirements/adminer/docker-entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -ex
-
-sed -i 's|^listen = .*|listen = 9000|' /etc/php/7.4/fpm/pool.d/www.conf
-chown -R www-data:www-data /var/www/html
-
-
-echo "Starting PHP-FPM..."
-exec php-fpm7.4 -F
